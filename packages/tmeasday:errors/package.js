18fc8d155cff6366714fd00a6b2ee0c9ce40b21c
Tom Coleman <tom@thesnail.org>
2013-04-10 13:57:55 +1000

    Created basic errors package and linked it in.
    sidebar9-5-1

---
+++ packages/tmeasday:errors/package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  summary: "A pattern to display application errors to the user",
+  version: "1.0.0"
+});
+
+Package.onUse(function (api, where) {
+  api.versionsFrom('METEOR@0.9.0');
+  
+  api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
+
+  api.add_files(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
+  
+  if (api.export) 
+    api.export('Errors');
+});
