5f6818735fe309c25dd3da5786466e5bee3fdfb0
Sacha Greif <sacha357@gmail.com>
2013-06-11 10:44:45 +0900

    Enabling secure mode.
    chapter17-3

--- client/main.js
+++ client/main.js
@@ -4,6 +4,8 @@ Deps.autorun(function(){
       email: Meteor.user().emails[0].address,
       created_at: Math.round(Meteor.user().createdAt/1000),
       user_name: Meteor.user().username,
+      user_id: Meteor.user()._id,
+      user_hash: Meteor.user().intercomHash,
       app_id: "k20iexvc"
     };
     Intercom('boot', intercomSettings);
--- packages/intercom/package.js
+++ packages/intercom/package.js
@@ -1,6 +1,10 @@
 Package.describe({
   summary: "Intercom package"
 });
+
 Package.on_use(function (api) {
   api.add_files('intercom_loader.js', 'client');
+  api.add_files('intercom_server.js', 'server');
+  
+  api.export('IntercomHash', 'server');
 });
